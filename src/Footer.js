bb34f43acc0534b40a390b8e2f26242fa2fa69f1
acskata <93493050+acskata@users.noreply.github.com>
2022-10-08 17:42:20 +0100

    Installed react, axios in order to display weather

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <small>
+        <a
+          href="https://github.com/acskata/vanilla-weather-app"
+          target="_blank" rel="noreferrer"
+        >
+          Open-sourced
+        </a>
+        <span> by Kata Acs</span>
+      </small>
+    </div>
+  );
+}
